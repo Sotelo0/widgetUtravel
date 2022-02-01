85eda6c4d6fed6bd9bf14bccf662650849d0b592
edgar-sb <edgar@space.bar>
2022-02-01 19:28:00 +0100

    cambios

--- src/pages/DetalleDeProducto/index.js
+++ src/pages/DetalleDeProducto/index.js
@@ -33,9 +33,9 @@ const DetalleDeProducto = () => {
     for (let i = 0; i < local.passager; i++) {
       passanger.push(
         <Card variant={'outlined'} style={{ padding: "2em" }}>
-          <Grid container spacing={2}>
-            <Grid item sm="3">Datos del pasajero :</Grid>
-            <Grid item sm={3}>
+          <Grid container spacing={4}>
+            <Grid item sm={2}>Datos del pasajero :</Grid>
+            <Grid item sm={2}>
               <label for={`pasajero numero ${i + 1} Nombre`} >Nombre *</label>
               <input style={{ width: "100%" }} required="true" id={`pasajero numero ${i + 1} Nombre`} onChange={(e) => {
                 let key = e.target.id;
@@ -43,15 +43,74 @@ const DetalleDeProducto = () => {
                 setData({ ...data, [key]: value })
               }} ></input>
             </Grid>
-            <Grid item sm={3}>
-              <label for={`pasajero numero ${i + 1} Fecha de nacimiento`} >Fecha de nacimiento *</label>
-              <input data-date-inline-picker="true" style={{ width: "100%" }} placeholder="Fecha de nacimiento" required="true" type="date" helperText="Fecha de nacimiento" id={`pasajero numero ${i + 1} Fecha de nacimiento`} onChange={(e) => {
-                let key = e.target.id;
-                let value = e.target.value;
-                setData({ ...data, [key]: value })
-              }}></input  >
+            <Grid item sm={6}>
+              <Grid container spacing={2}>
+                <Grid item sm={4}>
+                  <label for={`pasajero numero ${i + 1}  D`} >Dia *</label>
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                    <option value="16">16</option>
+                    <option value="17">17</option>
+                    <option value="18">18</option>
+                    <option value="19">19</option>
+                    <option value="20">20</option>
+                    <option value="21">21</option>
+                    <option value="22">22</option>
+                    <option value="23">23</option>
+                    <option value="24">24</option>
+                    <option value="25">25</option>
+                    <option value="26">26</option>
+                    <option value="27">27</option>
+                    <option value="28">28</option>
+                    <option value="29">29</option>
+                    <option value="30">30</option>
+                    <option value="31">31</option>
+
+
+                  </select>
+                </Grid>
+                <Grid item sm={4}>
+                  <label for={`pasajero numero ${i + 1}  M`} >Mes *</label>
+                  <select>
+                    <option value="Enero">Enero</option>
+                    <option value="Febrero">Febrero</option>
+                    <option value="Marzo">Marzo</option>
+                    <option value="Abril">Abril</option>
+                    <option  value="Mayo">Mayo</option>
+                    <option value="Junio">Junio</option> 
+
+                    <option value="Julio">Julio</option> 
+                    <option value="Agosto">Agosto</option> 
+                    <option value="Septiembre">Septiembre</option> 
+                    <option value="Octubre">Octubre</option> 
+                    <option value="Noviembre">Noviembre</option> 
+                    <option value="Diciembre">Diciembre</option> 
+
+                  </select>
+                </Grid>
+                <Grid item sm={4}>
+                  <label for={`pasajero numero ${i + 1}  A`} >Año *</label>
+                  <select>
+
+                  </select>
+                </Grid>
+              </Grid>
             </Grid>
-            <Grid item sm={3}>
+            <Grid item sm={2} >
               <label for={`pasajero numero ${i + 1} Sexo`}>Sexo *</label>
               <select style={{ width: "100%" }} required="true" helperText="Sexo" id={`pasajero numero ${i + 1} Sexo`} onChange={(e) => {
                 let key = e.target.id;
@@ -128,7 +187,7 @@ const DetalleDeProducto = () => {
     let dataPassager = reactLocalStorage.get('dataPassager')
     let dataPurific = dataPassager.replace(/\\/g, "").replace(/\}/g, "").replace(/\{/g, "").replace(/\"/g, "")
     console.log(dataPurific)
-   
+
     // var data = JSON.stringify({
     //   "quantity": 1,
     //   "name": "Seguro de viajero",
@@ -263,7 +322,7 @@ const DetalleDeProducto = () => {
                       </div>
                     </div>
                     <Grid item sm={12}>
-                      <Dialog open={open} width={700} style={{backgroundColor: "#000"}} onClose={handleClose}>
+                      <Dialog open={open} width={700} style={{ backgroundColor: "#000" }} onClose={handleClose}>
                         <iframe style={{ width: "100%", height: "100vh" }} src="https://irp.cdn-website.com/ece4ebdd/files/uploaded/Poliza.pdf"></iframe>
                       </Dialog>
                       {/* <a onClick={handleClickOpen}>Cobertura</a> */}
